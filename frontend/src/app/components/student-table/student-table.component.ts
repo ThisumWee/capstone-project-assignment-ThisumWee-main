099d8d3fd8dd3376f1a8166b3bd7551ef60806eb
ThisumWee <dwwatcdw@gmail.com>
2025-11-13 10:39:29 +0530

    Coding Updates

---
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { Router,NavigationExtras } from '@angular/router';
+import { faTrash, faPlus, faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import {AppServiceService} from '../../app-service.service';
+@Component({
+  selector: 'app-student-table',
+  templateUrl: './student-table.component.html',
+  styleUrls: ['./student-table.component.css']
+})
+export class StudentTableComponent implements OnInit {
+
+  faTrash = faTrash;
+  faPlus = faPlus;
+  faPenSquare = faPenSquare;
+  studentData: any;
+  selected: any;
+
+  constructor(private service : AppServiceService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getStudentData();
+  }
+
+  addNewStudent(){
+    this.router.navigate(['addStudent'])
+  }
+
+  editStudent(id: any){
+    const navigationExtras: NavigationExtras = {
+      state: {
+        id : id
+      }
+    };
+    this.router.navigate(['editStudent'], navigationExtras )
+  }
+
+  getStudentData(){
+    this.service.getStudentData().subscribe((response)=>{
+      this.studentData = Object.keys(response).map((key));
+    },(error)=>{
+      console.log('ERROR - ', error)
+    })
+  }
+
+  deleteStudent(itemid: any){
+    const student = {
+      id: itemid
+    }
+    this.service.deleteStudent(student).subscribe((response)=>{
+      this.getStudentData()
+    })
+  }
+
+  search(value: String) {
+    let foundItems: any[] = [];
+    if (value.length <= 0) {
+      this.getStudentData();
+    } else {
+      let b = this.studentData.filter((student: { name: { toLowerCase: () => String[]; }; }[]) => {
+        if (student[0].name.toLowerCase().indexOf(value) > -1) {
+          foundItems.push(student)
+        }
+      });
+      this.studentData = foundItems;
+    }
+  }
+}
+function key(value: string, index: number, array: string[]): unknown {
+  throw new Error('Function not implemented.');
+}
+
---
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { faTrash, faPlus, faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import { AppServiceService } from '../../app-service.service';
+@Component({
+  selector: 'app-teacher-table',
+  templateUrl: './teacher-table.component.html',
+  styleUrls: ['./teacher-table.component.css']
+})
+export class TeacherTableComponent implements OnInit {
+
+  faTrash = faTrash;
+  faPlus = faPlus;
+  faPenSquare = faPenSquare;
+  teacherData: any;
+  selected: any;
+
+  constructor(private service: AppServiceService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.getTeacherData();
+  }
+
+  addNewTeacher() {
+    this.router.navigate(['addTeacher'])
+  }
+
+  editTeacher(id: any) {
+    const navigationExtras: NavigationExtras = {
+      state: {
+        id: id
+      }
+    };
+    this.router.navigate(['editTeacher'], navigationExtras)
+  }
+
+  initializeDB(){
+    this.service.initializeDB().subscribe((response) => {
+      console.log('DB is Initialized')
+    }, (error) => {
+      console.log('ERROR - ', error)
+    })
+  }
+
+  getTeacherData() {
+    this.selected = 'Teachers';
+    this.service.getTeacherData().subscribe((response) => {
+      this.teacherData = Object.keys(response).map((key));
+    }, (error) => {
+      console.log('ERROR - ', error)
+    })
+  }
+
+  getStudentData() {
+    this.selected = 'Students';
+    this.service.getStudentData().subscribe((response) => {
+      this.teacherData = response;
+    }, (error) => {
+      console.log('ERROR - ', error)
+    })
+  }
+
+  search(value: string) {
+    let foundItems: any[] = [];
+    if (value.length <= 0) {
+      this.getTeacherData();
+    } else {
+      let b = this.teacherData.filter((teacher: { name: string; }[]) => {
+        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
+          foundItems.push(teacher)
+        }
+      });
+      this.teacherData = foundItems;
+    }
+  }
+
+  deleteTeacher(itemid: any) {
+    const test = {
+      id: itemid
+    }
+    this.service.deleteTeacher(test).subscribe((response) => {
+      this.getTeacherData()
+    })
+  }
+}
+
+function key(value: string, index: number, array: string[]): unknown {
+  throw new Error('Function not implemented.');
+}
